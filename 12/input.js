fae2dbcdc453321891c004cb152f08f4cbe8a889
Brian Cartan <cartanb@gmail.com>
2021-12-12 15:04:07 -0500

    day 12 start

---
+++ 12/input.js
@@ -0,0 +1,26 @@
+const input = `yb-start
+de-vd
+rj-yb
+rj-VP
+OC-de
+MU-de
+end-DN
+vd-end
+WK-vd
+rj-de
+DN-vd
+start-VP
+DN-yb
+vd-MU
+DN-rj
+de-VP
+yb-OC
+start-rj
+oa-MU
+yb-de
+oa-VP
+jv-MU
+yb-MU
+end-OC`;
+
+module.exports = input.split('\n');
